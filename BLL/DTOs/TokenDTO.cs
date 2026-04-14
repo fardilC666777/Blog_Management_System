6eb894c35c3a62cdf7642d398a45c9a59e027ed2
Fardil666777 <158137433+fardilC666777@users.noreply.github.com>
2026-04-14 22:53:11 +0600

    Add files via upload

---
+++ BLL/DTOs/TokenDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+   public class TokenDTO
+    {
+       
+        public string TKey { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeleteAt { get; set; }
+        
+        public string UserId { get; set; }
+    }
+}
